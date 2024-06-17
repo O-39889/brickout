264613d83e1c63118a80703d0b77c670dc744645
OLP Solutions <76152616+O-39889@users.noreply.github.com>
2024-06-18 00:20:39 +0300

    Add unbreakable brick scene

---
+++ scripts/IndestructibleBrick.gd
@@ -0,0 +1,11 @@
+class_name UnbreakableBrick extends "res://scripts/BrickBase.gd"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	super();
+	durability = -1;
+
+
+func hit(b: Ball):
+	print("Fat chance!");
