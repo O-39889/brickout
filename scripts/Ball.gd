1805b792cd0f82127bfe90afe22ee62e296214da
OLP Solutions <76152616+O-39889@users.noreply.github.com>
2024-06-17 18:00:52 +0300

    Make the balls move etc

--- scripts/Ball.gd
+++ scripts/Ball.gd
@@ -1,7 +1,23 @@
 extends CharacterBody2D
 
 
-const BALL_RADIUS = 25;
+enum BallSpeed {
+	BALL_SPEED_SLOW,
+	BALL_SPEED_NORMAL,
+	BALL_SPEED_FAST,
+}
+
+# random data fur now
+const BALL_SPEEDS = [
+	380,
+	500,
+	700,	
+];
+const BALL_RADIUS = 20;
+
+
+var speed := BALL_SPEEDS[BallSpeed.BALL_SPEED_NORMAL];
+var direction : Vector2 = Vector2(1, -1).normalized();
 
 
 @onready var collision_shape : CollisionShape2D = find_child("CollisionShape2D");
@@ -9,3 +25,20 @@ const BALL_RADIUS = 25;
 
 func _ready():
 	collision_shape.shape.radius = BALL_RADIUS;
+	launch();
+
+
+func launch():
+	velocity = direction * speed;
+
+
+func handle_collision(collision: KinematicCollision2D):
+	velocity = velocity.bounce(collision.get_normal());
+
+
+func _physics_process(delta):
+	var collision: KinematicCollision2D = move_and_collide(velocity * delta);
+	if collision:
+		handle_collision(collision);
+	else:
+		pass;
--- scripts/Level.gd
+++ scripts/Level.gd
@@ -1,34 +1,40 @@
 extends Node2D
 
 
-@onready var wall_right = find_child("WallRight");
-@onready var paddle = find_child("Paddle");
-
 var mouse_captured := false;
 
+@onready var wall_right = find_child("WallRight");
+@onready var paddle = find_child("Paddle");
+@onready var ball_packed = preload("res://scenes/Ball.tscn");
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	paddle.position.x = (get_viewport_rect().size.x - paddle.width) / 2;
 	paddle.position.y = get_viewport_rect().size.y - Globals.PADDLE_OFFSET;
-	
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func add_and_launch_ball(pos: Vector2, dir: Vector2 = Vector2(1,-1).normalized()):
+	var ball = ball_packed.instantiate();
+	ball.position = pos;
+	add_child(ball);
 
 
-func _physics_process(delta):
+func launch_ball(ball: CharacterBody2D, dir: Vector2 = Vector2(1,1).normalized()):
 	pass
 
 
 func _input(event):
+	if not mouse_captured:
+		if event is InputEventMouseButton:
+			if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+				add_and_launch_ball(event.position);
 	if event.is_action_pressed("debug_exit"):
 		get_tree().quit();
 	if event.is_action_pressed("debug_restart"):
 		set_mouse_capture(not mouse_captured);
+	if event.is_action_pressed("debug_do"):
+		pass
 
 
 func set_mouse_capture(captured: bool):
