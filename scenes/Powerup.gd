42509e37af07a3b94fc1aa8c3e210d31ce933f0a
OLP Solutions <76152616+O-39889@users.noreply.github.com>
2024-06-22 17:07:59 +0300

    Probably implement some simple powerup choice sustem

--- scenes/Powerup.gd
+++ scenes/Powerup.gd
@@ -1,13 +1,43 @@
 class_name Powerup extends CharacterBody2D
 
 
+const POWERUP_POOL = {
+	'good': [
+		'paddle_enlarge',
+		'add_ball',
+		'triple_ball',
+		'double_balls',
+		'sticky_paddle',
+		'barrier',
+		'fire_ball',
+		'acid_ball',
+	],
+	'neutral': [
+		'ball_speed_up',
+		'ball_slow_down',
+	],
+	'bad': [
+		'paddle_shrink',
+		'pop_ball',
+		'pop_all_balls',
+		'ghost_paddle',
+		'paddle_freeze',
+	],
+};
+
+
 const POWERUP_GRAVITY = 150;
 const INITIAL_SPEED = 200;
+
+
+var powerup : Dictionary;
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	velocity = get_launch_vector();
+	powerup = choose_powerup();
+	print(powerup);
 
 
 func _physics_process(delta):
@@ -43,6 +73,27 @@ func get_launch_vector() -> Vector2:
 		and rotate the initial speed vector by that angle
 	'''
 	return Vector2.UP * INITIAL_SPEED;
+
+
+func choose_powerup(weights: Dictionary = {}) -> Dictionary:
+	var pool := {};
+	if weights.is_empty():
+		pool["good"] = POWERUP_POOL["good"].duplicate();
+		pool["neutral"] = POWERUP_POOL['neutral'].duplicate();
+		pool['bad'] = POWERUP_POOL['bad'].duplicate();
+	var pool_sum : int = pool['good'].size() + pool['neutral'].size() + pool['bad'].size();
+	var choice := randi_range(0, pool_sum - 1);
+	var cum_weight := 0; # haha
+	var picked_key: String;
+	for p in pool:
+		cum_weight += pool[p].size();
+		if choice < cum_weight:
+			picked_key = p;
+			break;
+	return {
+		'type': picked_key,
+		'id': POWERUP_POOL[picked_key][randi_range(0, POWERUP_POOL[picked_key].size() - 1)],
+	};
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
