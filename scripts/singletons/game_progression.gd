7faa72670e882e7c6f667271c84e5ff07d5b6f8e
OLP Solutions <76152616+O-39889@users.noreply.github.com>
2024-07-22 00:07:00 +0300

    Add extra life every N points

--- scripts/singletons/game_progression.gd
+++ scripts/singletons/game_progression.gd
@@ -1,11 +1,17 @@
 extends Node;
 
 
+const EXTRA_LIFE_MULTIPLIER : int = 10000;
+
+
 @onready var score : int = 0:
 	get:
 		return score;
 	set(value):
 		score = maxi(value, 0);
+		if score >= (extra_lives_earned + 1) * EXTRA_LIFE_MULTIPLIER:
+			lives += 1;
+			extra_lives_earned += 1;
 		EventBus.score_changed.emit();
 
 @onready var lives : int = 3:
@@ -14,6 +20,8 @@ extends Node;
 	set(value):
 		lives = maxi(value, 0);
 		EventBus.lives_changed.emit();
+
+@onready var extra_lives_earned : int = score / EXTRA_LIFE_MULTIPLIER;
 
 
 func _ready():
