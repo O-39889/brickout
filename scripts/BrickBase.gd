07ac7c5decba2b3f17db1fab72607487ea2c5a6d
OLP Solutions <76152616+O-39889@users.noreply.github.com>
2024-08-07 17:25:05 +0300

    Add protected sides sprites and shimmering particles for some reason

--- scripts/BrickBase.gd
+++ scripts/BrickBase.gd
@@ -11,6 +11,14 @@ class_name Brick extends StaticBody2D;
 
 @onready var width : float = collision_shape.shape.size.x;
 @onready var height : float = collision_shape.shape.size.y;
+@onready var to_tween := [
+	{
+		'object': $Sprite2D,
+		'property': 'scale',
+		'final_val': Vector2(1, 1),
+		'duration': 0.2
+	},
+];
 
 
 func _ready():
@@ -20,7 +28,12 @@ func _ready():
 		+ global_position.y * (width / height) - height / 2) / 2220.2222)\
 		.timeout.connect(func():
 			var tween := create_tween();
-			tween.tween_property($Sprite2D, 'scale', Vector2(1, 1), 0.2);
+			for prop in to_tween:
+				var dur = prop['duration'] if prop.has('duration') else 0.2;
+				tween.tween_property(
+					prop['object'], prop['property'],
+					prop['final_val'], dur
+				);
 			);
 	
 
--- scripts/RegularBrick.gd
+++ scripts/RegularBrick.gd
@@ -10,6 +10,9 @@ enum Direction {
 };
 
 
+const ARMOR_TEXTURE := preload('res://assets/brick-edges-placeholder.png');
+
+
 ## Whether the brick should always provide a power-up when hit
 ## (tries to give out a good power-up).
 @export var is_shimmering : bool = false:
@@ -19,6 +22,12 @@ enum Direction {
 		if value == false and Engine.is_editor_hint():
 			$Sprite2D.modulate.a = 1.0;
 		is_shimmering = value;
+		if $GPUParticles2D:
+			$GPUParticles2D.visible = value;
+			$GPUParticles2D.emitting = value;
+			if not Engine.is_editor_hint():
+				if not value:
+					$GPUParticles2D.queue_free();
 
 ## Color of the brick, also determines the overall durability of the brick.
 @export_enum(
@@ -40,8 +49,7 @@ enum Direction {
 			return color;
 		set(value):
 			color = value;
-			if Engine.is_editor_hint():
-				queue_redraw();
+			update_sprites();
 
 ## Sides of the brick protected by armor; the brick can only be hit
 ## with a regular ball from the unprotected sides.
@@ -52,6 +60,8 @@ enum Direction {
 		protected_sides = value;
 		if Engine.is_editor_hint():
 			queue_redraw();
+		else:
+			set_armor_sprites();
 
 
 @onready var initial_durability : int = get_durability(color);
@@ -67,6 +77,15 @@ enum Direction {
 
 
 func _ready():
+	if is_shimmering:
+		if not Engine.is_editor_hint():
+			$GPUParticles2D.amount_ratio = 0.0;
+			to_tween.append({
+				'object': $GPUParticles2D,
+				'property': 'amount_ratio',
+				'final_val': 1.0,
+				'duration': 0.1,
+			});
 	super();
 	if initial_durability == 1 and not Engine.is_editor_hint():
 		$CrackSprite.queue_free();
@@ -76,7 +95,22 @@ func _ready():
 		if not Engine.is_editor_hint():
 			# to prevent softlocks or idk lol
 			self.remove_from_group(&'destructible_bricks');
-	queue_redraw();
+	update_sprites();
+	if is_reinforced:
+		if Engine.is_editor_hint():
+			queue_redraw();
+		else:
+			set_armor_sprites();
+	if $GPUParticles2D:
+		$GPUParticles2D.visible = is_shimmering;
+		$GPUParticles2D.emitting = is_shimmering;
+	if Engine.is_editor_hint():
+		$GPUParticles2D.amount = 5;
+	elif not Engine.is_editor_hint():
+		if not is_shimmering:
+			$GPUParticles2D.queue_free();
+		else:
+			$GPUParticles2D.amount = 10;
 
 
 func get_durability(color_idx : int) -> int:
@@ -91,6 +125,71 @@ func get_durability(color_idx : int) -> int:
 	if color_idx < 8:
 		return 5;
 	return 1; # fallback lmao
+
+
+func update_sprites():
+	$Sprite2D.region_rect = Rect2(
+				Vector2(0, height) * color,
+				Vector2(width, height)
+			);
+	if not Engine.is_editor_hint():
+		set_crack_sprite(durability, initial_durability);
+
+
+func set_armor_sprites():
+	if is_reinforced and not Engine.is_editor_hint():
+		var sprites_node : Node2D = $ArmorSprites;
+		for child in sprites_node.get_children():
+			child.queue_free();
+		
+		var sides := [];
+		var corners := [];
+		# have to do one by one by hand since trying to be a smartass
+		# didn't work because of some bullshit
+		# UPD: sorry guys I just mixed up width and height
+		# but alas, the old one is gone
+		# it was literally bulkier than this one though
+		if protected_sides & Direction.Top:
+			sides.append(0);
+		if protected_sides & Direction.Right:
+			sides.append(1);
+		if protected_sides & Direction.Bottom:
+			sides.append(2);
+		if protected_sides & Direction.Left:
+			sides.append(3);
+		if 0 in sides and 1 in sides:
+			corners.append(0);
+		if 1 in sides and 2 in sides:
+			corners.append(1);
+		if 2 in sides and 3 in sides:
+			corners.append(2);
+		if 3 in sides and 0 in sides:
+			corners.append(3);
+	
+		for side_idx in sides:
+			var sprite := Sprite2D.new();
+			sprite.region_enabled = true;
+			sprite.region_rect.position.x = 0;
+			sprite.region_rect.position.y = height * side_idx;
+			sprite.region_rect.size.x = width;
+			sprite.region_rect.size.y = height;
+			sprite.centered = true;
+			sprite.texture = ARMOR_TEXTURE;
+			sprite.name = 'Side' + 'NESW'[side_idx];
+			sprites_node.add_child(sprite);
+		for corner_idx in corners:
+			var sprite := Sprite2D.new();
+			sprite.region_enabled = true;
+			sprite.region_rect.position.x = width;
+			sprite.region_rect.position.y = height * corner_idx;
+			sprite.region_rect.size.x = width;
+			sprite.region_rect.size.y = height;
+			sprite.centered = true;
+			sprite.texture = ARMOR_TEXTURE;
+			sprite.name = 'Corner' + [
+				'NE', 'SE', 'SW', 'NW'
+			][corner_idx]
+			sprites_node.add_child(sprite);
 
 
 func set_crack_sprite(new_durability: int, initial_durability: int):
@@ -166,14 +265,21 @@ func hit(by: Node2D, damage: int):
 	else:
 		EventBus.brick_hit.emit(self, by);
 		if damage != 0:
+			if $GPUParticles2D:
+				$GPUParticles2D.amount_ratio -= 1.0 / (initial_durability + 1);
 			if crack_sprite:
 				crack_sprite.visible = true;
 				set_crack_sprite(durability, initial_durability);
-			queue_redraw();
 
 
 func destroy(by: Node2D):
 	EventBus.brick_destroyed.emit(self, by);
+	if $GPUParticles2D:
+		var particles : GPUParticles2D = $GPUParticles2D;
+		particles.reparent(get_parent());
+		(particles as GPUParticles2D).lifetime /= 2.0;
+		particles.emitting = false;
+		get_tree().create_timer(1).timeout.connect(func(): particles.queue_free());
 	queue_free();
 
 
@@ -182,8 +288,31 @@ func get_points() -> int:
 
 
 func _process(delta):
-	if is_shimmering:
-		$Sprite2D.modulate.a = sin(Time.get_ticks_msec() / 42) / 2 + 0.5;
+	#if is_shimmering:
+		#$Sprite2D.modulate.a = sin(Time.get_ticks_msec() / 42) / 2 + 0.5;
+	pass
+
+
+func _draw():
+	if Engine.is_editor_hint():
+		if is_reinforced:
+			# TODO: have this shit actually draw the armor on top of it
+				if protected_sides & Direction.Top:
+					draw_rect(Rect2(
+						-width / 2, -height / 2, width, 10
+					), Color.BLACK, true);
+				if protected_sides & Direction.Right:
+					draw_rect(Rect2(
+						width / 2 - 10, -height / 2, 10, height
+					), Color.BLACK, true);
+				if protected_sides & Direction.Bottom:
+					draw_rect(Rect2(
+						-width / 2, height / 2 - 10, width, 10
+					), Color.BLACK, true);
+				if protected_sides & Direction.Left:
+					draw_rect(Rect2(
+						-width / 2, -height / 2, 10, height
+					), Color.BLACK, true);
 
 
 func _angle_diff(normal: Vector2, dir: Direction) -> float:
@@ -198,33 +327,3 @@ func _angle_diff(normal: Vector2, dir: Direction) -> float:
 		Direction.Left:
 			vec = Vector2.LEFT;
 	return rad_to_deg(angle_difference(normal.angle(), vec.angle()));
-
-
-func _draw():
-	$CollisionShape2D.visible = color == null;
-	if color != null:
-		$Sprite2D.region_rect = Rect2(
-			Vector2(0, height) * color, Vector2(width, height)
-		);
-	if is_reinforced:
-		if protected_sides & Direction.Top:
-			draw_rect(Rect2(
-				-width / 2, -height / 2, width, 10
-			), Color.BLACK, true);
-		if protected_sides & Direction.Right:
-			draw_rect(Rect2(
-				width / 2 - 10, -height / 2, 10, height
-			), Color.BLACK, true);
-		if protected_sides & Direction.Bottom:
-			draw_rect(Rect2(
-				-width / 2, height / 2 - 10, width, 10
-			), Color.BLACK, true);
-		if protected_sides & Direction.Left:
-			draw_rect(Rect2(
-				-width / 2, -height / 2, 10, height
-			), Color.BLACK, true);
-	if not Engine.is_editor_hint():
-		if color == 0 or color == 3: # cyan or yellow
-			self_modulate = Color.BLACK;
-		draw_string(ThemeDB.fallback_font, Vector2(0,0), str(durability),0,-1,
-		28);
