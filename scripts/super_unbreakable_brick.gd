2caf5e99aebeca1c8abb1beff7f12a4751e14133
OLP Solutions <76152616+O-39889@users.noreply.github.com>
2024-08-09 20:54:22 +0300

    Add sprites to unbreakable bricks

--- scripts/super_unbreakable_brick.gd
+++ scripts/super_unbreakable_brick.gd
@@ -2,5 +2,11 @@
 class_name SuperUnbreakableBrick extends Brick;
 
 
+func _ready():
+	$Sprite2D.region_rect.size.x = width;
+	$Sprite2D.region_rect.size.y = height;
+	super();
+
+
 func hit(by: Node2D, _damage: int):
 	EventBus.brick_hit.emit(self, by);
--- scripts/unbreakable_brick.gd
+++ scripts/unbreakable_brick.gd
@@ -2,6 +2,12 @@
 class_name UnbreakableBrick extends Brick;
 
 
+func _ready():
+	$Sprite2D.region_rect.size.x = width;
+	$Sprite2D.region_rect.size.y = height;
+	super();
+
+
 func hit(by: Node2D, damage: int):
 	if damage > 1:
 		EventBus.brick_destroyed.emit(self, by);
