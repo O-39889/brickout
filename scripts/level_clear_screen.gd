29f2660bb42f6ed6043474a63f06adcb6d416bef
OLP Solutions <76152616+O-39889@users.noreply.github.com>
2025-01-04 00:12:59 +0300

    Procrastinate by making another obscure feature
    ?????

--- scripts/level_clear_screen.gd
+++ scripts/level_clear_screen.gd
@@ -3,8 +3,13 @@ class_name LevelClearScreen extends Control;
 
 @onready var score_lbl : Label = %ScoreLbl;
 @onready var time_lbl : Label = %TimeLbl;
+@onready var tip_lbl : Label = %Tip;
 @onready var continue_btn : Button = %ContinueBtn;
 @onready var exit_btn : Button = %ExitBtn;
+
+
+func _ready() -> void:
+	tip_lbl.queue_free();
 
 
 # NOTE: in the level clear screen, time should be the score in the current level,
