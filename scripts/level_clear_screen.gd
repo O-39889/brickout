9871dedc3db9d53202de46a7e99857619311e586
OLP Solutions <76152616+O-39889@users.noreply.github.com>
2025-01-04 15:04:40 +0300

    Work more on the level clear screen tips for some reason

--- scripts/level_clear_screen.gd
+++ scripts/level_clear_screen.gd
@@ -8,9 +8,48 @@ class_name LevelClearScreen extends Control;
 @onready var exit_btn : Button = %ExitBtn;
 
 
+# TODO: use this to prevent repeating tips
+# | 1024 for the rare ones
+static var last_tip_idx := -1;
+static var tip_dict := ResourceLoader.load("res://data/loading_screen_tips.json").data as Dictionary;
+
+
 func _ready() -> void:
+	_set_tip();
+	return;
 	tip_lbl.queue_free();
 
+
+func _set_tip() -> void:
+	var tip_text : String;
+	if randf() < 0.01:
+		# use rare tip
+		#var rare_idx := int(randf() * tip_dict['rare'].size()) # nvm for now
+		tip_text = tip_dict['rare'].pick_random();
+	else:
+		# otherwise, use common tip
+		tip_text = tip_dict['common'].pick_random();
+		pass
+	tip_text = tip_text.replace('EXTRA_LIFE_MULTIPLIER',
+		str(GameProgression.EXTRA_LIFE_MULTIPLIER))\
+		.replace('WHEREVER_THE_HECK_ARE_THEY_DISPLAYED',
+		'the level clear screen')\
+		.replace('OS_NAME', OS.get_name())\
+		.replace('OS_JUDGEMENTAL_TEXT',
+		"I've yet to come up with customized judgemental messages for every supported OS though")\
+		.replace('№',
+		(func() -> String:
+			var counter := 0;
+			for tip : String in tip_dict['common']:
+				counter += tip.countn('ball');
+			for tip : String in tip_dict['rare']:
+				counter += tip.countn('ball');
+			return str(counter);
+			).call());
+	if not(tip_text.begins_with('Fun fact')
+	or tip_text.begins_with('News')):
+		tip_text = 'Tip: ' + tip_text;
+	tip_lbl.text = tip_text;
 
 # NOTE: in the level clear screen, time should be the score in the current level,
 # not the total score
